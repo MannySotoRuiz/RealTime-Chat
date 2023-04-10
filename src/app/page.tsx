d98364ead4252301a51d171a906cd3b11ce2c4df
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-04-10 14:58:36 -0400

    added next auth code for google singin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { db } from "@/lib/db"
+import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
 export default async function Home() {
   
-  await db.set("hello", "hello");
-
   return (
-    <div className='text-red-500'>hello world</div>
-  )
+    <Button variant={"ghost"}>Hello</Button>
+  );
 }
