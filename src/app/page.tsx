d5786119ab3699e80ae9363fe2a1396f173bc8eb
Emmanuel Soto Ruiz <emmanuelsotoruiz@emmanuels-mbp.mynetworksettings.com>
2023-04-09 23:47:21 -0400

    integrated the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { db } from "@/lib/db"
+
+export default async function Home() {
+  
+  await db.set("hello", "hello");
+
   return (
     <div className='text-red-500'>hello world</div>
   )
