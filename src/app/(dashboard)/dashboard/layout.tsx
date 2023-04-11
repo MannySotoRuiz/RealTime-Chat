5310d996acc27bb3c1b9a2a0e13b85dcf79aa9a7
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-04-11 17:50:13 -0400

    made ui changes, added container to tailwind container

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -115,7 +115,9 @@ const Layout = async ({ children }: LayoutProps) => {
                     </ul>
                 </nav>
             </div>
-            {children}
+            <aside className="max-h-screen container py-16 md:py-12 w-full">
+                {children}
+            </aside>
         </div>
     );
 }
