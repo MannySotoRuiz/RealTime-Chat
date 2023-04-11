0bfbaef2fdf32202f6297a122aa959f10a0bc49f
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-04-11 19:05:30 -0400

    fixed toast bug and not updating other user's friend request with new ones

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -36,6 +38,9 @@ export async function POST(req: Request) {
         if (!hasFriendRequest) {
             return new Response('No friend request', { status: 400 });
         }
+
+        // notify added user if online
+        pusherServer.trigger(toPusherKey(`user:${idToAdd}:friends`), 'new_friend', {});
 
         // both ppl accepting friend requests
         await db.sadd(`user:${session.user.id}:friends`, idToAdd);
--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -65,6 +65,9 @@ const SidebarChatList: FC<SidebarChatListProps> = ({friends, sessionId}) => {
         return () => {
             pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`)); // stop listening
             pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`)); 
+
+            pusherClient.unbind('new_message', chatHandler);
+            pusherClient.unbind('new_friend', newFriendHandler);
         }
     }, [pathname, sessionId, router]);
 
