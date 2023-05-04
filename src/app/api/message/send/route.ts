5a7fbaef8d64fa463439079a1100c1cdbc3537a2
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-05-04 18:01:42 -0400

    production bug, realtime pusher trigger events needed await

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -8,71 +8,78 @@ import { nanoid } from "nanoid";
 import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
-    try {
-        const { text, chatId }: { text: string, chatId: string } = await req.json();
-        const session = await getServerSession(authOptions);
+  try {
+    const { text, chatId }: { text: string; chatId: string } = await req.json();
+    const session = await getServerSession(authOptions);
 
-        if (!session) return new Response('Unauthorized', { status: 401 });
+    if (!session) return new Response("Unauthorized", { status: 401 });
 
-        const [userId1, userId2] = chatId.split('--');
+    const [userId1, userId2] = chatId.split("--");
 
-        if (session.user.id !== userId1 && session.user.id !== userId2) {
-            return new Response('Unauthorized', { status: 401 });
-        }
-
-        const friendId = session.user.id === userId1 ? userId2 : userId1;
-
-        const friendList = await fetchRedis(
-            'smembers', 
-            `user:${session.user.id}:friends`
-        ) as string[];
-        const isFriend = friendList.includes(friendId);
-
-        if (!isFriend) {
-            return new Response('Unauthorized', { status: 401 });
-        }
-
-        const rawSender = await fetchRedis(
-            'get', 
-            `user:${session.user.id}`
-        ) as string;
-        const sender = JSON.parse(rawSender) as User;
-
-        const timestamp = Date.now();
-
-        const messageData: Message = {
-            id: nanoid(),
-            senderId: session.user.id,
-            text,
-            timestamp,
-        }
-
-        const message = messageValidator.parse(messageData);
-
-        // notify all connected chat room clients
-        pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message); // this message is received from front end (messageHandler funciton)
-        
-        // now on every msg we send, for the user triggering a new event
-        pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
-            ...message, 
-            senderImg: sender.image,
-            senderName: sender.name
-        });
-
-        // all valid, send the message
-        await db.zadd(`chat:${chatId}:messages`, {
-            score: timestamp,
-            member: JSON.stringify(message)
-        });
-        // set is sorted by timestamp
-
-        return new Response('OK');
-
-    } catch (error) {
-        if (error instanceof Error) {
-            return new Response(error.message, { status: 500 });
-        }
-
-        return new Response('Internal Server Error', { status: 500 });
+    if (session.user.id !== userId1 && session.user.id !== userId2) {
+      return new Response("Unauthorized", { status: 401 });
     }
+
+    const friendId = session.user.id === userId1 ? userId2 : userId1;
+
+    const friendList = (await fetchRedis(
+      "smembers",
+      `user:${session.user.id}:friends`
+    )) as string[];
+    const isFriend = friendList.includes(friendId);
+
+    if (!isFriend) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const rawSender = (await fetchRedis(
+      "get",
+      `user:${session.user.id}`
+    )) as string;
+    const sender = JSON.parse(rawSender) as User;
+
+    const timestamp = Date.now();
+
+    const messageData: Message = {
+      id: nanoid(),
+      senderId: session.user.id,
+      text,
+      timestamp,
+    };
+
+    const message = messageValidator.parse(messageData);
+
+    // notify all connected chat room clients
+    await pusherServer.trigger(
+      toPusherKey(`chat:${chatId}`),
+      "incoming-message",
+      message
+    ); // this message is received from front end (messageHandler funciton)
+
+    // now on every msg we send, for the user triggering a new event
+    await pusherServer.trigger(
+      toPusherKey(`user:${friendId}:chats`),
+      "new_message",
+      {
+        ...message,
+        senderImg: sender.image,
+        senderName: sender.name,
+      }
+    );
+
+    // all valid, send the message
+    await db.zadd(`chat:${chatId}:messages`, {
+      score: timestamp,
+      member: JSON.stringify(message),
+    });
+    // set is sorted by timestamp
+
+    return new Response("OK");
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 });
+    }
+
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,102 +1,129 @@
 "use client";
 
-import { pusherClient } from '@/lib/pusher';
-import { cn, toPusherKey } from '@/lib/utils';
-import { Message } from '@/lib/validations/message';
-import { format } from 'date-fns';
-import Image from 'next/image';
-import { FC, useEffect, useRef, useState } from 'react';
+import { pusherClient } from "@/lib/pusher";
+import { cn, toPusherKey } from "@/lib/utils";
+import { Message } from "@/lib/validations/message";
+import { format } from "date-fns";
+import Image from "next/image";
+import { FC, useEffect, useRef, useState } from "react";
 
 interface MessagesProps {
-    initialMessages: Message[]
-    sessionId: string
-    chatId: string
-    sessionImg: string | null | undefined
-    chatPartner: User
+  initialMessages: Message[];
+  sessionId: string;
+  chatId: string;
+  sessionImg: string | null | undefined;
+  chatPartner: User;
 }
 
 const Messages: FC<MessagesProps> = ({
-    initialMessages,
-    sessionId,
-    chatId,
-    sessionImg,
-    chatPartner
+  initialMessages,
+  sessionId,
+  chatId,
+  sessionImg,
+  chatPartner,
 }) => {
+  // when a user sends a message, we can put into the state, showing it directly to the user instead of having to refresh the page
+  const [messages, setMessages] = useState<Message[]>(initialMessages);
+  const scrollDownRef = useRef<HTMLDivElement | null>(null);
 
-    // when a user sends a message, we can put into the state, showing it directly to the user instead of having to refresh the page
-    const [messages, setMessages] = useState<Message[]>(initialMessages);
-    const scrollDownRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    // doesnt allow back ticks so need to use function for that
+    pusherClient.subscribe(toPusherKey(`chat:${chatId}`)); // now listening
 
-    useEffect(() => {
-        // doesnt allow back ticks so need to use function for that
-        pusherClient.subscribe(toPusherKey(`chat:${chatId}`)); // now listening 
+    // the message will get passed from the server
+    const messageHandler = (message: Message) => {
+      setMessages((prev) => [message, ...prev]); // get access to what they were previously
+    };
 
-        // the message will get passed from the server
-        const messageHandler = (message: Message) => {
-            setMessages((prev) => [message, ...prev]); // get access to what they were previously
-        }
+    // now tell pusher what to do when something occurs
+    pusherClient.bind("incoming-message", messageHandler);
 
-        // now tell pusher what to do when something occurs
-        pusherClient.bind('incoming-message', messageHandler);
+    return () => {
+      pusherClient.unsubscribe(toPusherKey(`chat:${chatId}`));
+      pusherClient.unbind("incoming-message", messageHandler);
+    };
+  }, [chatId]);
 
-        return () => {
-            pusherClient.unsubscribe(toPusherKey(`chat:${chatId}`));
-            pusherClient.unbind('incoming-message', messageHandler);
-        }
-    }, [chatId]);
+  const formatTimestamp = (timestamp: number) => {
+    return format(timestamp, "HH:mm");
+  };
 
-    const formatTimestamp = (timestamp: number) => {
-        return format(timestamp, 'HH:mm');
-    }
+  return (
+    <div
+      id="messages"
+      className="flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+    >
+      {/* we created this div at the top of the component*/}
+      {/* first of we reversed the order, everything is reveresed */}
+      {/* secondly, when we send a message, we automatically want to scroll to that message (by using the ref)*/}
+      <div ref={scrollDownRef} />
 
-    return (
-        <div id='messages' className='flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
-            {/* we created this div at the top of the component*/}
-            {/* first of we reversed the order, everything is reveresed */}
-            {/* secondly, when we send a message, we automatically want to scroll to that message (by using the ref)*/}
-            <div ref={scrollDownRef} />
+      {messages.map((message, idx) => {
+        const isCurrentUser = message.senderId === sessionId;
 
-            {messages.map((message, idx) => {
-                const isCurrentUser = message.senderId === sessionId
+        // we need to know if there is a next msg from the same user
+        const hasNextMessageFromSameUser =
+          messages[idx - 1]?.senderId === messages[idx].senderId;
 
-                // we need to know if there is a next msg from the same user
-                const hasNextMessageFromSameUser = messages[idx - 1]?.senderId === messages[idx].senderId;
+        return (
+          <div
+            key={`${message.id}-${message.timestamp}`}
+            className="chat-message"
+          >
+            <div
+              className={cn("flex items-end", {
+                "justify-end": isCurrentUser,
+              })}
+            >
+              <div
+                className={cn(
+                  "flex flex-col space-y-2 text-base max-w-xs mx-2",
+                  {
+                    "order-1 items-end": isCurrentUser,
+                    "order-2 items-start": !isCurrentUser,
+                  }
+                )}
+              >
+                <span
+                  className={cn("px-4 py-2 rounded-lg inline-block", {
+                    "bg-indigo-600 text-white": isCurrentUser,
+                    "bg-gray-200 text-gray-900": !isCurrentUser,
+                    "rounded-br-none":
+                      !hasNextMessageFromSameUser && isCurrentUser, // only the last message that is sent concurrently from the same user has an endge border and others will be round
+                    "rounded-bl-none":
+                      !hasNextMessageFromSameUser && !isCurrentUser,
+                  })}
+                >
+                  {message.text}{" "}
+                  <span className="ml-2 text-xs text-gray-400">
+                    {formatTimestamp(message.timestamp)}
+                  </span>
+                </span>
+              </div>
 
-                return (
-                    <div key={`${message.id}-${message.timestamp}`} className='chat-message'>
-                        <div className={cn('flex items-end', {
-                            'justify-end': isCurrentUser,
-                        })}>
-                            <div className={cn('flex flex-col space-y-2 text-base max-w-xs mx-2', {
-                                'order-1 items-end': isCurrentUser,
-                                'order-2 items-start': !isCurrentUser,
-                            })}>
-                                <span className={cn('px-4 py-2 rounded-lg inline-block', {
-                                    'bg-indigo-600 text-white': isCurrentUser,
-                                    'bg-gray-200 text-gray-900': !isCurrentUser,
-                                    'rounded-br-none': !hasNextMessageFromSameUser && isCurrentUser, // only the last message that is sent concurrently from the same user has an endge border and others will be round 
-                                    'rounded-bl-none': !hasNextMessageFromSameUser && !isCurrentUser,
-                                })}>
-                                    {message.text}{' '}
-                                    <span className='ml-2 text-xs text-gray-400'>
-                                        {formatTimestamp(message.timestamp)}
-                                    </span>
-                                </span>
-                            </div>
-
-                            <div className={cn('relative w-6 h-6', {
-                                'order-2': isCurrentUser,
-                                'order-1': !isCurrentUser,
-                                'invisible': hasNextMessageFromSameUser,
-                            })}>
-                                <Image fill src={isCurrentUser ? (sessionImg as string) : chatPartner.image} alt="Profile picture" referrerPolicy='no-referrer' className='rounded-full' />
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
-}
+              <div
+                className={cn("relative w-6 h-6", {
+                  "order-2": isCurrentUser,
+                  "order-1": !isCurrentUser,
+                  invisible: hasNextMessageFromSameUser,
+                })}
+              >
+                <Image
+                  fill
+                  src={
+                    isCurrentUser ? (sessionImg as string) : chatPartner.image
+                  }
+                  alt="Profile picture"
+                  referrerPolicy="no-referrer"
+                  className="rounded-full"
+                />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Messages;
